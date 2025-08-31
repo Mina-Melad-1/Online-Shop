9506ac5095b465af4b12a6dadaf95f73b2f3f285
Mina-Melad-1 <menamelad586@gmail.com>
2025-09-01 01:11:37 +0300

    implement home page

--- index.php
+++ index.php
@@ -3,9 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/master.css">
     <title>Document</title>
 </head>
 <body>
-        Welcome to the homepage!
+       <header class="header">
+            <a href="index.php">shop</a>
+            <a href="index.php"><img src="" alt=""></a>
+            <nav>
+                <ul>
+                    <li><a href="">login</a></li>
+                    <li><a href="">signup</a></li>
+                    <li><a href="">dashboard</a></li>
+                </ul>
+            </nav>
+       </header>
+       <main class="main">
+        <?php for($i=0; $i<15; $i++){ ?>
+            <div class="product">
+                <img src="productimg/shoe1.jpg" alt="productimg">
+                <h2>product title</h2>
+                <p>product description</p>
+                <p>product quantity</p>
+                <p class="productprice">product price</p>
+                <a href="#">Buy Now</a>
+            </div>
+            <?php } ?>
+       </main>
+       <footer class="footer">
+            <p>copyright@: webtech knowledge </p>
+       </footer>
 </body>
 </html>
