8ef74743d043e2cc14fca662528fb40df6792f61
Mina-Melad-1 <menamelad586@gmail.com>
2025-09-07 00:44:25 +0300

    Add product into database

--- admin/addproduct.php
+++ admin/addproduct.php
@@ -1,8 +1,31 @@
 <?php 
 session_start();
+include '../db.php';
 if(isset($_SESSION['user_id'])){
+    $sql1 = "select * from categories";
+    $result1 = mysqli_query($conn, $sql1);
     if($_SESSION['user_role'] == 'admin') {
-        
+        if(isset($_POST['submit'])) {
+            // Get form data
+            $name = $_POST['name'];
+            $price = $_POST['price'];
+            $stock = $_POST['stock'];
+            $description = $_POST['description'];
+            $category_name = $_POST['category_name'];
+            $image = $_FILES['image']['name'];
+            $temp_location = $_FILES['image']['tmp_name'];
+            $upload_locaton = "../image/";
+
+            $sql = "INSERT INTO products (name, price, stock, description,category_name,image) VALUES ('$name', '$price', '$stock', '$description', '$category_name', '$image')";
+
+            $result = mysqli_query($conn, $sql);
+            if(!$result) {
+                $error = "Error:  {$conn->error}";
+            } else {
+                move_uploaded_file($temp_location, $upload_locaton . $image);
+                $success = "Product added successfully.";
+            }
+        }
     } else {
         echo "Access denied. You are not an admin.";
     }
@@ -30,16 +53,22 @@ else{
     <div class="dashboard_main">
         <h1>Add Product</h1>
         <form action="addproduct.php" method="post" enctype="multipart/form-data">
+            <?php if(isset($error)): ?>
+                <div class="error"><?php echo $error; ?></div>
+            <?php endif; ?>
+            <?php if(isset($success)): ?>
+                <div class="success"><?php echo $success; ?></div>
+            <?php endif; ?>
+            <br>
             <input type="text" name="name" placeholder="Product Name" required>
             <input type="number" name="price" placeholder="Product Price" required>
             <input type="number" name="stock" placeholder="Product stock" required>
             <input type="file" name="image" required>
             <textarea name="description" placeholder="Product Description" required></textarea>
-            <select name="" required>
-                <option value="category_id">category_id</option>
-            </select>
-            <select name="" required>
-                <option value="category_name">category_name</option>
+            <select name="category_name" required>
+                <?php while($row = mysqli_fetch_assoc($result1) ){ ?>
+                <option value="<?php echo $row['name'];?>"><?php echo $row['name'];?></option>
+                <?php } ?>
             </select>
             <input type="submit" name="submit" value="Add Product">
         </form>
